98473794c1f93a52d77d06bfc23b43b791467c1e
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-03-08 12:29:48 +0530

    Update spotify.py

--- userbot/helpers/spotify.py
+++ userbot/helpers/spotify.py
@@ -2,7 +2,7 @@ import spotipy
 import spotipy.util as util
 from spotipy import SpotifyException
 
-from userbot import SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from Bonten import SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
 redirect_uri = "http://localhost:8888/callback"
 scope = 'user-read-currently-playing app-remote-control'
