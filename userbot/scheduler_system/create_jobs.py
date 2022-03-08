f5cbdc236ed02051d516844f2bb7feda9890db21
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-03-08 12:31:42 +0530

    Update create_jobs.py

--- userbot/scheduler_system/create_jobs.py
+++ userbot/scheduler_system/create_jobs.py
@@ -1,14 +1,13 @@
 
-import userbot
-from userbot import UserBot, scheduler
+import Bonten
+from Bonten import Bonten, scheduler
 
 
 
 def set_client(new_client):
-    userbot.client = new_client
+    Bonten.client = new_client
 
 
 def add_job(function, seconds=3):
-    scheduler.add_job(function, "interval", seconds=seconds, args=[userbot.client])
-
+    scheduler.add_job(function, "interval", seconds=seconds, args=[Bonten.client])
 
