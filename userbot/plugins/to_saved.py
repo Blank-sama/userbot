8d5e659f40bf316bda48a53168c653e5b46814f2
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-03-08 12:40:05 +0530

    Update to_saved.py

--- userbot/plugins/to_saved.py
+++ userbot/plugins/to_saved.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from userbot import UserBot
-from userbot.plugins.help import add_command_help
+from Bonten import Bonten
+from Bonten.plugins.help import add_command_help
 
 
-@UserBot.on_message(filters.command("s", ".") & filters.me)
+@Bonten.on_message(filters.command("s", ".") & filters.me)
 async def to_saved(_, message: Message):
     await message.delete()
     await message.reply_to_message.forward("self")
